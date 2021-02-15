e4d3dd1ccf99ac04e04349e27a608658e74fd30b
Kerlyn <kerlynava97@gmail.com>
2021-02-14 22:36:39 -0600

    [ADD] Tree views | Calendar

--- opac/models/session.py
+++ opac/models/session.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
 from odoo import models, fields, api, exceptions
 
 class Session(models.Model):
@@ -15,6 +18,8 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -40,7 +45,28 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
-            
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            r.duration = (r.end_date - r.start_date).days + 1
+      
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for r in self:
